a88e88379c18941dd0172c68bca2a5dc3bca7aba
castrolol <lu4n.k4str0@gmail.com>
2016-06-03 03:42:22 -0400

    Added Interactions with CalendarView
     - DayClick
     - WeekClick (need revision)
     - DayLongClick
     - WeekLongClick (need revision)

---
+++ controls/src/main/java/com/castrolol/githidra/calendarcontrols/calendarview/interfaces/OnDayLongClickListener.java
@@ -0,0 +1,8 @@
+package com.castrolol.githidra.calendarcontrols.calendarview.interfaces;
+
+/**
+ * Created by 'Luan on 03/06/2016.
+ */
+
+public interface OnDayLongClickListener {
+}
---
+++ controls/src/main/java/com/castrolol/githidra/calendarcontrols/calendarview/interfaces/OnWeekClickListener.java
@@ -0,0 +1,15 @@
+package com.castrolol.githidra.calendarcontrols.calendarview.interfaces;
+
+import android.view.View;
+
+import com.castrolol.githidra.calendarcontrols.calendarview.model.CalendarDay;
+
+/**
+ * Created by 'Luan on 03/06/2016.
+ */
+
+public interface OnWeekClickListener {
+
+    void onClick(View view, CalendarDay day);
+
+}
